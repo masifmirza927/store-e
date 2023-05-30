c6d3b2e004d2510ccaaa5d803976f18cdeb7981e
masifmirza927 <masifmirza927@gmail.com>
2023-05-30 17:39:32 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+
+    fetch('https://fakestoreapi.com/products').then((res) => {
+      return res.json()
+    }).then((data) => {
+      setProducts(data);
+      console.log(data);
+    })
+
+  }, []);
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className='row'>
+      {
+          products.map((product) => {
+            return (
+              <div className='col-md-3' style={{ marginBottom: "10px" }}>
+                <div className="card">
+                  <img src={product.image} className="card-img-top" alt="..." style={{width: "100%", height: "140px"}}  />
+                  <div className="card-body">
+                    <h5 className="card-title text-truncate">{product.title}</h5>
+                    <p className="card-text"> ${product.price}</p>
+                    <button  className="btn btn-primary mx-2">View Details</button>
+                    <button  className="btn btn-primary">Add to cart</button>
+                  </div>
+                </div>
+              </div>
+            )
+          })
+        }
+      </div>
     </div>
   );
 }
